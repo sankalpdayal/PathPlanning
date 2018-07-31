e957b40b67ce9cff9d3bf22931a52b41631b4e59
Sankalp Dayal <sadayal@invensense.com>
2018-07-30 17:29:14 -0700

    Fixing bugs

--- src/main.cpp
+++ src/main.cpp
@@ -204,12 +204,12 @@ int main() {
   int lane = 1;
   
   //define reference valocity
-  double ref_val = 49.5;
+  double ref_vel = 49.5;
   
   //define max velocity
   //double max_val = 49.5;
   
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&ref_val,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&ref_vel,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -304,7 +304,7 @@ int main() {
 				double ref_x_prev = previous_path_x[prev_size-2];
 				double ref_y_prev = previous_path_y[prev_size-2];
 				
-				ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev)
+				ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
 				
 				ptsx.push_back(ref_x_prev);
 				ptsx.push_back(ref_x);
